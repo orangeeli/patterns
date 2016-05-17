81930ea009c15106565ab163386c8b3696d2721f
Eliseu Martinho <eliseu.martinho@gmail.com>
2016-05-17 06:29:39 +0000

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+(()=> {
+  
+  'use strict';
+
+  const gulp = require('gulp'),
+    mocha = require('gulp-mocha');
+
+  const tasks = {
+
+    mocha (){
+      return gulp.src(['./test/*.js'], {read: false})
+
+        .pipe(mocha({
+          reporter: 'nyan',
+          recursive: true
+        })).once('error', function () {
+          process.exit(1);
+        });
+    }
+
+  };
+
+  gulp.task('test:mocha', tasks.mocha);
+  gulp.task('default', ['test:mocha']);
+  
+})();
